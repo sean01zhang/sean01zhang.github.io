4d66d45b9ad19ca766f452f119e4b7b64e46ef7e
Sean Zhang <20467017+sean01zhang@users.noreply.github.com>
2023-03-06 00:03:10 -0500

    ray tracing part 1 finished

--- projects/graphics/util/vec3.h
+++ projects/graphics/util/vec3.h
@@ -134,4 +134,13 @@ vec3 refract(const vec3 &uv, const vec3 &n, double eta_i_over_eta_t) {
   vec3 r_out_para = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
   return r_out_perp + r_out_para;
 }
+
+vec3 random_in_unit_disk() {
+  while (true) {
+    auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+    if (p.length_squared() <= 1) return p;
+  }
+}
+
+
 #endif
